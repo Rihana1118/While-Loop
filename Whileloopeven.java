5c2e2b2004ac456d789baa719a6b8affe8f7cb2d
Rihana1118 <rihanashariff18@gmail.com>
2024-11-21 10:48:45 +0530

    Add files via upload

---
+++ Whileloopeven.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Whileloopeven {
+
+	public static void main(String[] args) {
+	int i=2, sum=0;
+	while (i<=100) {
+		sum+=i;
+		i=i+2;
+	}
+	System.out.println("The sum of even numbers 1 to 100 :"+sum);
+	}
+
+}
